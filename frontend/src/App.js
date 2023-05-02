d769ec4dedb9d60cc222147244de00cd015b2a0b
BizCommmIntl <BizCommIntl381@gmail.com>
2023-05-02 21:01:31 +0500

    refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { createContext } from "react"
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 import { ChildA } from "./Pages/ChildA";
 import { ChildB } from "./Pages/ChildB";
@@ -6,28 +7,35 @@ import { ChildB } from "./Pages/ChildB";
 //import MyTestPrinting from './MyTestPrinting';
 import './App.css';
 import AppLogo from './Market.gif'
-import Products from "./Pages/Products";
+import Home from "./Pages/Home";
+import Product from "./Pages/Product";
 
 export const GLOBAL_data = createContext()
 
 function App() {
   return (
     <>
+      <BrowserRouter>
         <div className="App-header">
           <div>
-                      <img src={AppLogo} alt="" height={30} width={100} />
-          Market
+            <Link to={`/`}>
+              <img src={AppLogo} alt="" height={30} width={100} />
+            </Link>
+
+            My Super Market
           </div>
           <div>
-            link1 
+            link1
             link2
           </div>
         </div>
 
-        <div className="main">
-          <Products />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Product/:slug' element={<Product />} />
+        </Routes>
 
-        </div>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/Pages/Products.jsx
+++ frontend/src/Pages/Products.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { data } from '../data.js'
+import { Link } from 'react-router-dom'
 
 export default function Products() {
     return (
         <div>
             <p>Products Catalogue</p>
-            {data.products.map ((E, I) => 
-                <div className="card">
-                    <img src={E.image} alt="" />
-                    <p>{E.name}</p>
-                    <p>{E.brand}</p>
-                    <p>{E.price}</p>
-                </div>
-            )
-            }
+
+            <div className="products">
+                {data.products.map((E, I) =>
+                    <div className="product">
+                        <Link to={`/Product/${E.slug}`}>
+                            <img src={E.image} alt="" />
+                        </Link>
+                        <div> <Link to={`/Product/${E.slug}`}> {E.name}</Link></div>
+                        <div><strong>{E.brand}</strong></div>
+                        <div>Rs. {E.price}</div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
